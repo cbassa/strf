978536c53c3448273494088bc7badcd4aa908cd9
Fabian P. Schmidt <kerel@mailbox.org>
2021-11-20 15:48:53 +0100

    Add config.sh
    Signed-off-by: Fabian P. Schmidt <kerel@mailbox.org>

--- config.sh
+++ config.sh
@@ -1,1 +1,24 @@
-../sattools/config.sh
+# very opioninated configuration file by and for kerel
+
+# KEEP IN SYNC WITH stvid/configuration.ini !!!!
+
+# export PGPLOT_DIR=/usr/local/src/pgplot-5.2.2
+export PGPLOT_FONT=/usr/lib/grfont.dat
+
+source ../strf/.env
+
+SATTOOLS=$SATNOGS_DIR/sattools
+
+export ST_COSPAR
+export ST_DATADIR=$SATNOGS_DIR/data/
+export ST_TLEDIR=$SATNOGS_DIR/data/tles/sattools
+export ST_OBSDIR=$SATNOGS_DIR/data/satobs
+
+# Only on dev branch:
+export ST_SITES=$SATNOGS_DIR/data/data/sites.txt
+
+# space-track.org credentials
+export ST_LOGIN
+
+PATH=$PATH:$SATTOOLS
+export PATH=$PATH:$SATTOOLS/scripts
