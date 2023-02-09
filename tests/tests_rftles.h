92cebaa923fbb243124ace3d4c40496f8af39462
Martin Herren <martin.herren@gmail.com>
2023-02-09 21:40:20 +0100

    [Tests] Add initial unit tests on rftles based on cmocka framework
    Signed-off-by: Martin Herren (HB9FXX) <cq@hb9fxx.ch>

---
+++ tests/tests_rftles.h
@@ -0,0 +1,14 @@
+#ifndef _TESTS_RFTLES_H
+#define _TESTS_RFTLES_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int run_tle_tests();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _TESTS_RFTLES_H */
