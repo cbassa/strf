2aaaa6e10ee46cdfdbe768eb30599085e18faedd
Martin Herren (HB9FXX) <cq@hb9fxx.ch>
2023-08-05 21:14:44 +0200

    [rffft] Add -P option to parse params from SatDump and GQRX filenames
    Signed-off-by: Martin Herren (HB9FXX) <cq@hb9fxx.ch>

---
+++ rffft_internal.h
@@ -0,0 +1,23 @@
+#ifndef _RFFFT_INTERNAL_H
+#define _RFFFT_INTERNAL_H
+
+#include "sgdp4h.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// input:
+// filename: filename string to parse
+// output:
+// samplerate: parsed samplerate
+// frequency: parsed frequency
+// format: parsed sample format: char: 'c', int: 'i', float: 'f'
+// starttime: parsed start time string formatted YYYY-MM-DDTHH:MM:SS.sss
+int rffft_params_from_filename(char * filename, double * samplerate, double * frequency, char * format, char * starttime);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _RFFFT_INTERNAL_H */
--- tests/tests.c
+++ tests/tests.c
@@ -1,3 +1,4 @@
+#include "tests_rffft_internal.h"
 #include "tests_rftles.h"
 
 #include <stdarg.h>
@@ -9,6 +10,7 @@
 int main(void) {
   int failures = 0;
 
+  failures += run_rffft_internal_tests();
   failures += run_tle_tests();
 
   return failures;
