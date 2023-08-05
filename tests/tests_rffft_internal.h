d782dbc58b1a276f1b2714d2bbe4e340bde6fa20
Martin Herren (HB9FXX) <cq@hb9fxx.ch>
2023-08-05 21:14:44 +0200

    [rffft] Add -P option to parse params from SatDump and GQRX filenames
    Signed-off-by: Martin Herren (HB9FXX) <cq@hb9fxx.ch>

---
+++ tests/tests_rffft_internal.h
@@ -0,0 +1,14 @@
+#ifndef _TESTS_RFFFT_INTERNAL_H
+#define _TESTS_RFFFT_INTERNAL_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int run_rffft_internal_tests();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _TESTS_RFFFT_INTERNAL_H */
