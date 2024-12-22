ad9c9ca713c5b0e611959f85b4a683839f376e29
Cees Bassa <cbassa@gmail.com>
2024-12-22 14:51:41 +0100

    Working version of alpha5 conversion
    Signed-off-by: Cees Bassa <cbassa@gmail.com>

--- alpha5.c
+++ alpha5.c
@@ -1,51 +1,104 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
 
+// Fixed mapping
+const char mapping[] = "0123456789ABCDEFGHJKLMNPQRSTUVWXYZ";
 
-int alpha5_to_number(char *s)
+// Function to convert alpha5 format to number
+int alpha5_to_number(const char *s)
 {
-  int i;
-  char c,spart[]="1234";
-  char alpha5[]="ABCDEFGHJKLMNPQRSTUVWXYZ";
-  
-  printf("%c %s %d\n",alpha5[0],alpha5,strlen(alpha5));
+    // Split components
+    char first_char = s[0];
+    int digits = 0;
+    sscanf(s + 1, "%4d", &digits);  // Get the last four digits
+    
+    // Decode first character
+    int first_digit = 0;
+    if (first_char == ' ') {
+        first_digit = 0;
+    } else if (isdigit(first_char)) {
+        first_digit = first_char - '0';
+    } else {
+        for (int i = 0; mapping[i] != '\0'; i++) {
+            if (mapping[i] == first_char) {
+                first_digit = i;
+                break;
+            }
+        }
+    }
 
-  c = s[0];
-  strncpy(spart,s+1,5);
-  spart[5]='\0';
-  for (i=0;i<strlen(alpha5);i++) 
-    if (c==alpha5[i])
-      break;
-
-  printf(">> %d %c |%c| |%s|\n",i,c,alpha5[i],spart);
-  return (i+10)*10000+atoi(spart);
+    // Create and return number
+    return first_digit * 10000 + digits;
 }
 
-void number_to_alpha5(int number,char *s)
+// Function to convert number to alpha5 format
+void number_to_alpha5(int number, char *result)
 {
-  int i;
-  char c,alpha5[]="ABCDEFGHJKLMNPQRSTUVWXYZ";
+    // Split components
+    int first_digit = number / 10000;
+    int digits = number % 10000;
 
-  i=number/10000;
-  c=alpha5[i-10];
-  sprintf(s,"%c%04d",c,number-i*10000);
+    // Get char
+    char first_char = mapping[first_digit];
 
-  return;
+    // Create string
+    sprintf(result, "%c%04d", first_char, digits);
+
+    return;
 }
 
-int main(int argc, char *argv[])
+// Function to strip leading spaces
+void strip_leading_spaces(const char *s, char *result)
 {
-  int number;
-  char s[] = "A0000",st[]="     ";
+    while (*s == ' ') {
+        s++;  // Skip leading spaces
+    }
+    strcpy(result, s);  // Copy the remainder of the string
 
-  number = alpha5_to_number(s);
+    return;
+}
 
-  printf("%d\n",number);
+// Function to zero pad a string
+void zero_pad(const char *s, char *result)
+{
+    char stripped[6];  // Temporarily store the string without leading spaces
+    strip_leading_spaces(s, stripped);
 
-  number_to_alpha5(number,st);
+    if (isdigit(stripped[0])) {
+        int value = atoi(stripped);
+        sprintf(result, "%05d", value);
+    } else {
+        strcpy(result, stripped);  // No zero-padding for non-digit strings
+    }
+}
 
-  printf("%s\n",st);
-  
-  return 0;
+int main() {
+    const char *norad_strings[] = {"00001", "    1", " 1234", "05697", "12345", "A0000", "J2931", "W1928", "E8493", "P4018", "Z9999"};
+    const int correct_numbers[] = {1, 1, 1234, 5697, 12345, 100000, 182931, 301928, 148493, 234018, 339999};
+    
+    for (int i = 0; i < 11; i++) {
+        const char *norad_string = norad_strings[i];
+        int correct_number = correct_numbers[i];
+
+        int number = alpha5_to_number(norad_string);
+        printf("|%s| -> |%6d|==|%6d| %d\n", norad_string, number, correct_number, number == correct_number);
+    }
+
+    printf("\n");
+
+    for (int i = 0; i < 11; i++) {
+        int correct_number = correct_numbers[i];
+        char s[7];  // For storing the alpha5 string
+
+        number_to_alpha5(correct_number, s);
+        
+        char zero_padded[6];  // For storing the zero-padded string
+        zero_pad(norad_strings[i], zero_padded);
+
+        printf("|%6d| -> |%s|==|%s| %d\n", correct_number, s, zero_padded, strcmp(s, zero_padded) == 0);
+    }
+
+    return 0;
 }
